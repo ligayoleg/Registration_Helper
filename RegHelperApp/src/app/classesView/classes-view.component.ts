68a1479736db7583bb5994e0684fa90d3a1aafc7
Oleg Ligay <ligayoleg@gmail.com>
2020-08-02 16:22:42 -0400

    added cars tutorial

--- RegHelperApp/src/app/classesView/classes-view.component.ts
+++ RegHelperApp/src/app/classesView/classes-view.component.ts
@@ -1,27 +1,84 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Car } from '../car';
+import { CarService } from '../carservice';
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-classes-view',
   templateUrl: './classes-view.component.html',
-  styleUrls: ['./classes-view.component.scss']
+  styleUrls: ['./classes-view.component.scss'],
 })
 export class ClassesViewComponent implements OnInit {
+  displayDialog: boolean;
 
-  addedClasses;
+  car: Car = {};
+
+  selectedCar: Car;
+
+  newCar: boolean;
+
+  cars: Car[];
+
+  cols: any[];
 
   days: any[];
-  constructor() { }
+
+  constructor(private carService: CarService) {}
 
   ngOnInit(): void {
+    this.carService.getCarsSmall().then((cars) => (this.cars = cars));
+
     this.days = [
-      {label: "Monday", value: "mon"},
-      {label: "Tuesday", value: "tue"},
-      {label: "Wednesday", value: "wed"},
-      {label: "Thursday", value: "thu"},
-      {label: "Friday", value: "fri"},
-      {label: "Saturday", value: "sat"},
-      {label: "Sunday", value: "sun"},
-    ]
+      { label: 'Monday', value: 'mon' },
+      { label: 'Tuesday', value: 'tue' },
+      { label: 'Wednesday', value: 'wed' },
+      { label: 'Thursday', value: 'thu' },
+      { label: 'Friday', value: 'fri' },
+      { label: 'Saturday', value: 'sat' },
+      { label: 'Sunday', value: 'sun' },
+    ];
+
+    this.cols = [
+      { field: 'vin', header: 'Vin' },
+      { field: 'year', header: 'Year' },
+      { field: 'brand', header: 'Brand' },
+      { field: 'color', header: 'Color' },
+    ];
   }
 
+  showDialogToAdd() {
+    this.newCar = true;
+    this.car = {};
+    this.displayDialog = true;
+  }
+
+  save() {
+    let cars = [...this.cars];
+    if (this.newCar) cars.push(this.car);
+    else cars[this.cars.indexOf(this.selectedCar)] = this.car;
+
+    this.cars = cars;
+    this.car = null;
+    this.displayDialog = false;
+  }
+
+  delete() {
+    let index = this.cars.indexOf(this.selectedCar);
+    this.cars = this.cars.filter((val, i) => i != index);
+    this.car = null;
+    this.displayDialog = false;
+  }
+
+  onRowSelect(event) {
+    this.newCar = false;
+    this.car = this.cloneCar(event.data);
+    this.displayDialog = true;
+  }
+
+  cloneCar(c: Car): Car {
+    let car = {};
+    for (let prop in c) {
+      car[prop] = c[prop];
+    }
+    return car;
+  }
 }
