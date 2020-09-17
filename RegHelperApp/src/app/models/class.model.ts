85dd43d2b739216e1ddcc54f42aef0d3b0fe889e
Oleg Ligay <ligayoleg@gmail.com>
2020-09-16 20:31:36 -0400

    changed the meny

--- RegHelperApp/src/app/models/class.model.ts
+++ RegHelperApp/src/app/models/class.model.ts
@@ -11,3 +11,10 @@ export interface Subject {
   dayTwoTimeStart?: string;
   dayTwoTimeFinish?: string;
 }
+
+export interface Event {
+  id?: number;
+  name?: string;
+  type?: string;
+  days?: [];
+}
--- RegHelperApp/src/app/navigation/navigation.component.ts
+++ RegHelperApp/src/app/navigation/navigation.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
-import {MenuItem} from 'primeng/api';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   links: MenuItem[];
 
   ngOnInit() {
-      this.links = [
-          {label: 'Home', routerLink: ['/homeView']},
-          {label: 'Events', routerLink: ['/eventsView']},
-          {label: 'Calendar', routerLink: ['/calendarView']},
-      ];
+    this.links = [
+      { label: 'Home', routerLink: ['/homeView'], icon: 'pi pi-home' },
+      { label: 'Events', routerLink: ['/eventsView'], icon: 'pi pi-th-large' },
+      {
+        label: 'Calendar',
+        routerLink: ['/calendarView'],
+        icon: 'pi pi-calendar',
+      },
+    ];
   }
-
 }
